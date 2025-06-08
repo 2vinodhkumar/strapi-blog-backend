3ea1cf7af134a9fea8096b7b4324e7e295e03936
2vinodhkumar <2vinodhkumar@gmail.com>
2025-06-08 12:16:52 -0500

    Initial Strapi setup

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['mUpqXgu/f8SEcJe5av/vJw==','zOE/8GmQZZAhMKQ4H7FAbw==','ONt/oWRIwuH4NBkivY5t3g==','+T++p7hEiQ7g3zkySZLzLg==']),
   },
 });
