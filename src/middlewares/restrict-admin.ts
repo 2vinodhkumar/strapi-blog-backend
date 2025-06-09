f68a5c2ef2aa64d2893f2428e7f06a21c4be794c
2vinodhkumar <2vinodhkumar@gmail.com>
2025-06-08 21:54:43 -0500

    Initial Strapi setup

--- src/middlewares/restrict-admin.ts
+++ src/middlewares/restrict-admin.ts
@@ -1,10 +1,10 @@
 const allowedIps = ['47.222.12.40'];
 export default (config, { strapi }) => {
   return async (ctx, next) => {
-    const allowedIps = ['::1', '127.0.0.1', 'YOUR_PUBLIC_IP'];
+    const allowedIps = ['::1', '127.0.0.1', '47.222.12.40'];
 
 const ip = ctx.request.ip.replace('::ffff:', '');
-
+console.log('IP Address:', ip);
 if (
   ctx.request.url.startsWith('/admin') &&
   process.env.NODE_ENV === 'production' &&
