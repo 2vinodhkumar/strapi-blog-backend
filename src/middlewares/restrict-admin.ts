b571003bf90c3c5f0b4779838d5f5ce50d386233
2vinodhkumar <2vinodhkumar@gmail.com>
2025-06-08 21:39:17 -0500

    Initial Strapi setup

--- src/middlewares/restrict-admin.ts
+++ src/middlewares/restrict-admin.ts
@@ -1,10 +1,11 @@
 const allowedIps = ['47.222.12.40'];
 export default (config, { strapi }) => {
   return async (ctx, next) => {
+    const ip = ctx.request.headers['x-forwarded-for'] || ctx.request.ip;
     if (
     ctx.request.url.startsWith('/admin') &&
     process.env.NODE_ENV === 'production' &&
-    !allowedIps.includes(ctx.request.ip)
+    !allowedIps.includes(ip)
     ) {
     ctx.unauthorized(`Admin access is disabled for your IP`);
     }
