cda983fc9ec942e101faa6cfaa3d3e372adaefa4
2vinodhkumar <2vinodhkumar@gmail.com>
2025-06-08 22:00:22 -0500

    Initial Strapi setup

--- src/middlewares/restrict-admin.ts
+++ src/middlewares/restrict-admin.ts
@@ -5,6 +5,11 @@ export default (config, { strapi }) => {
 
 const ip = ctx.request.ip.replace('::ffff:', '');
 console.log('IP Address:', ip);
+const rawIp = ctx.request.headers['x-forwarded-for'] || ctx.request.ip;
+const clientIp = Array.isArray(rawIp) ? rawIp[0] : rawIp?.toString().split(',')[0] || '';
+
+// Now use `clientIp` instead of `ctx.request.ip`
+console.log('Client IP:', clientIp);
 if (
   ctx.request.url.startsWith('/admin') &&
   process.env.NODE_ENV === 'production' &&
